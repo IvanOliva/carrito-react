f17d1f528129aea0abbf9f49fc071df658b1651b
ivan <ioliva@ingeros.com>
2021-02-05 10:24:35 -0300

    Add redux-persist to project, and save cart reducer into localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
+
 import { logger } from 'redux-logger';
 import rootReducers from './root-reducer'
 
 const middleWares = [logger];
 
-const store = createStore(rootReducers, applyMiddleware(...middleWares));
+export const store = createStore(rootReducers, applyMiddleware(...middleWares));
 
-export default store; 
+export const persistor = persistStore(store);
+
+export default { store, persistor }; 
